1a3757dc9356f9245ad0e263afff2706120b32aa
ICON39 <164856080+ICON39@users.noreply.github.com>
2025-01-08 10:56:05 +0100

    Update admin_dashboard.php

--- php/admin_dashboard.php
+++ php/admin_dashboard.php
@@ -8,48 +8,6 @@ if (!isset($_SESSION['username']) || $_SESSION['ruolo'] !== 'admin') {
     exit();
 }
 
-// funzione che mostra tutti gli utenti del file xml che hanno ruolo 'richiesta_admin'
-function mostraRichiesteAdmin() {
-    $xml_file = '../xml/utenti.xml';
-    $xml = simplexml_load_file($xml_file);
-    
-    $username = $xml->xpath("//utente[ruolo='richiesta_admin']/username"); // array di username
-    
-    // stampa un form per ogni utente con ruolo 'richiesta_admin'
-    echo "<form action='admin_dashboard.php' method='post'>";
-    echo "<select name='username'>";
-    foreach ($username as $user) {
-        echo "<option value='$user'>$user</option>";
-    }
-    echo "</select>";
-    echo "<button type='submit' name='accetta'>Accetta</button>";
-    echo "<button type='submit' name='rifiuta'>Rifiuta</button>";
-    echo "</form>";
-
-}
-
-// se si preme il pulsante 'rifiuta', cambia il ruolo dell'utente selezionato in 'cliente'
-if (isset($_POST['rifiuta'])) {
-    $xml_file = '../xml/utenti.xml';
-    $xml = simplexml_load_file($xml_file);
-    $username = $_POST['username'];
-    $utente = $xml->xpath("//utente[username='$username']")[0];
-    $utente->ruolo = 'cliente';
-    $xml->asXML($xml_file);
-    header("Location: admin_dashboard.php");
-    exit();
-}
-// se si preme il pulsante 'accetta', cambia il ruolo dell'utente selezionato in 'admin'
-if (isset($_POST['accetta'])) {
-    $xml_file = '../xml/utenti.xml';
-    $xml = simplexml_load_file($xml_file);
-    $username = $_POST['username'];
-    $utente = $xml->xpath("//utente[username='$username']")[0];
-    $utente->ruolo = 'admin';
-    $xml->asXML($xml_file);
-    header("Location: admin_dashboard.php");
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +21,5 @@ if (isset($_POST['accetta'])) {
     <h1>Benvenuto nella Dashboard Admin</h1>
     <p>Qui puoi gestire le richieste di crediti, le FAQ, ecc.</p>
     <h3>Richieste Admin</h3>
-    <?php mostraRichiesteAdmin(); ?>
-    <a href="logout.php">Logout</a>
 </body>
 </html>
